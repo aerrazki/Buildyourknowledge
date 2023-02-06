6660ea81193cc3e76f68fd5265c340b09a2e0686
ayoub <ayoub@MSI>
2023-02-06 23:16:21 +0100

    Add project files.

---
+++ BuildYourKnowledge/Model/Question.cs
@@ -0,0 +1,17 @@
+﻿namespace BuildYourKnowledge.Model
+{
+    public class Question
+    {
+        public Question()
+        {
+
+        }
+        public Question(int id) { }
+        public int _Id { get; set; }
+        public string _Question { get; set; }
+
+        public string[] Choices { get; set; }
+
+        public string[] Answers { get; set; }
+    }
+}
